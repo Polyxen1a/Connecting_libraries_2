c43cf9b9e19e0ec0374603a886b2b29edbc5d8e0
Polyxen1a <maslowa@inbox.ru>
2023-02-04 22:59:43 +0300

    Initial commit

---
+++ src/main/java/com/skypro/connecting_libraries_2/ConnectingLibraries2Application.java
@@ -0,0 +1,13 @@
+package com.skypro.connecting_libraries_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingLibraries2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingLibraries2Application.class, args);
+	}
+
+}
